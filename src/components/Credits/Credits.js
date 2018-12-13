2f3da681cba7fbaf003ded2370e7cd0712b4218d
Arber Braja <admin@arbraja.com>
2018-12-13 20:33:50 +0100

    Small Changes

--- src/components/Credits/Credits.js
+++ src/components/Credits/Credits.js
@@ -3,7 +3,7 @@ import React from 'react';
 const credits = (props) => {
     return (
         <div className="credits">
-            <p>Handcrafted by <a href="https://www.arbraja.com/" target="_blank" rel="noopener noreferrer" title="Arber Braja - Frontend Web Developer Albania">Arber Braja</a></p>
+            <p>Handcrafted by <a href="https://www.arbraja.com/" target="_blank" rel="noopener noreferrer" title="Arber Braja - Frontend Web Developer">Arber Braja</a></p>
         </div>
     );
 };
