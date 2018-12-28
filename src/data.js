ff0004175ff9ec60f6ac7f8b7ba6307046413605
Arber Braja <admin@arbraja.com>
2018-12-28 10:11:59 +0100

    Fixing feeds issues

--- src/data.js
+++ src/data.js
@@ -47,12 +47,5 @@ export const FEEDS = [
 		name: 'Deepsense AI',
 		url: 'https://deepsense.ai/wp-json/wp/v2/posts?per_page=10',
 		chosenStatus: false
-	},
-	{
-		id: 8,
-		link: 'tympanus.net/codrops',
-		name: 'Codrops',
-		url: 'https://tympanus.net/codrops/wp-json/wp/v2/posts?per_page=10',
-		chosenStatus: false
 	}
 ];
