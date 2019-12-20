29367e1eec6326512a94d40b45e016d42dcf64f2
Arber Braja <admin@arbraja.com>
2019-12-20 16:49:15 +0100

    Bug solved, issue on data.js (one of the sources was not working ok). Async comp loading needs to be fixed with lazy and suspense now

--- src/data.js
+++ src/data.js
@@ -8,58 +8,44 @@ export const FEEDS = [
 	},
 	{
 		id: 2,
-		link: 'blog.sucuri.net',
-		name: 'Sucuri Blog',
-		url: 'https://blog.sucuri.net/wp-json/wp/v2/posts?per_page=10',
-		chosenStatus: false
-	},
-	{
-		id: 3,
 		link: 'kinsta.com',
 		name: 'Kinsta',
 		url: 'https://kinsta.com/wp-json/wp/v2/posts?per_page=10',
 		chosenStatus: false
 	},
 	{
-		id: 4,
+		id: 3,
 		link: 'codeinwp.com',
 		name: 'CodeinWP',
 		url: 'https://www.codeinwp.com/wp-json/wp/v2/posts?per_page=10',
 		chosenStatus: false
 	},
 	{
-		id: 5,
+		id: 4,
 		link: 'deliciousbrains.com',
 		name: 'Delicious Brains',
 		url: 'https://deliciousbrains.com/wp-json/wp/v2/posts?per_page=10',
 		chosenStatus: false
 	},
 	{
-		id: 6,
+		id: 5,
 		link: 'valentinog.com',
 		name: 'Valentino Gagliardi',
 		url: 'https://www.valentinog.com/blog/wp-json/wp/v2/posts?per_page=10',
 		chosenStatus: false
 	},
 	{
-		id: 7,
+		id: 6,
 		link: 'davidwalsh.name',
 		name: 'David Walsh Blog',
 		url: 'https://davidwalsh.name/wp-json/wp/v2/posts?per_page=10',
 		chosenStatus: false
 	},
 	{
-		id: 8,
+		id: 7,
 		link: 'appendto.com',
 		name: 'appendTo',
 		url: 'https://appendto.com/wp-json/wp/v2/posts?per_page=10',
-		chosenStatus: false
-	},
-	{
-		id: 9,
-		link: 'appdividend.com',
-		name: 'AppDividend',
-		url: 'https://appdividend.com/wp-json/wp/v2/posts?per_page=10',
 		chosenStatus: false
 	}
 ];
